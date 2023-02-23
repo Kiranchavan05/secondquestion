f8aba617ab10a2629e100767e947a65682f562bb
Kiranchavan05 <kiranchavan0502@gmail.com>
2023-02-23 15:13:29 +0530

    third

---
+++ dom5thque.js
@@ -0,0 +1,9 @@
+var li=document.getElementsByTagName('li')
+console.log(li);
+
+li[2].textContent='kiranchavan';
+li[3].style.fontWeight='bold';
+li[1].style.color='green';
+//li.style.color='red'   =>error
+
+//we cannot change additional list with TagName and Classname
